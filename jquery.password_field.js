2ebf6884c8f433d7737618b5a5dc229df5526371
Barnabas Sudy <barnabas@beekeeper.io>
2016-06-07 13:37:46 +0200

    FUL-2540: Added initial version of password strength indicator logic

--- jquery.password_field.js
+++ jquery.password_field.js
@@ -64,6 +64,93 @@
             });
 
         });
+
     }
 
+    $.fn.strengthIndicator = function(options) {
+
+        var settings = $.extend({
+            strength: {
+                week: 'Week',
+                soso: 'So-so',
+                okay: 'Okay',
+                good: 'Good',
+                strong: 'Strong',
+                na: ''
+            },
+            validity: {
+                header: 'Your password must have',
+                pwd_length: '8 or more characters',
+                pwd_upper_and_lower: 'Upper & lowercase letters',
+                pwd_digits: 'At least one number'
+            }
+        }, options);
+
+        this.each(function () {
+            var $this = $(this);
+
+            var $fragment = $('<div><div class="password-strength"> \
+                    <ul class="password-strength-indicator"> \
+                        <li class="strong good okay so-so week"></li> \
+                        <li class="strong good okay so-so"></li> \
+                        <li class="strong good okay"></li> \
+                        <li class="strong good"></li> \
+                        <li class="strong"></li> \
+                    </ul> \
+                    <div class="password-strength-text"><span></span></div> \
+                </div> \
+                <div class="password-validity-header">' + settings.validity['header'] + '</div> \
+                <ul class="password-validity"> \
+                    <li class="length">' + settings.validity['pwd_length'] + '</li> \
+                    <li class="upper-and-lower">' + settings.validity['pwd_upper_and_lower'] + '</li> \
+                    <li class="digits">' + settings.validity['pwd_digits'] + '</li> \
+                </ul></div>');
+
+            $fragment.insertAfter($this);
+
+
+            $this.on('input', function(event) {
+
+
+                var password = $this.val();
+
+                var hasDigit = /\d/.test(password);
+                var hasLower = /[a-z]/.test(password);
+                var hasUpper = /[A-Z]/.test(password);
+
+                $fragment.find('.password-validity .digits').toggleClass('valid', hasDigit);
+                $fragment.find('.password-validity .upper-and-lower').toggleClass('valid', hasLower && hasUpper);
+                $fragment.find('.password-validity .length').toggleClass('valid', password.length >= 8);
+
+                var result = zxcvbn(password);
+
+                console.log(result.score);
+
+                var strength = null;
+                if (password) {
+                    switch (result.score) {
+                        case 4:
+                            strength = 'strong';
+                            break;
+                        case 3:
+                            strength = 'good';
+                            break;
+                        case 2:
+                            strength = 'okay';
+                            break;
+                        case 1:
+                            strength = 'soso';
+                            break;
+                        default:
+                            strength = 'week';
+                    }
+                } else {
+                    strength = 'na';
+                }
+
+                $fragment.find('.password-strength').attr('data-strength', strength);
+                $fragment.find('.password-strength-text').html(settings.strength[strength]);
+            });
+        })
+    }
 }(jQuery));
